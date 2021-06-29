9ffa3ecb85a34db0c33ab6ce0af3dd0291a15ad1
Lena <serbova@itstep.academy>
2021-06-29 07:47:08 +0300

    Add blog init

---
+++ MVCRouting/MVCRouting/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MVCRouting.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BlogContext _blogContext;
+
+        public HomeController(ILogger<HomeController> logger, BlogContext blogContext)
+        {
+            _logger = logger;
+            _blogContext = blogContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _blogContext.Posts.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(string articleName)
+        {
+            return View(await _blogContext.Posts
+                .Include(p=>p.PostTags)
+                .ThenInclude(pt=>pt.Tag)
+                .FirstOrDefaultAsync(p=>p.Title == articleName));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCRouting/MVCRouting/Transformers/SlugifyParameterTransformer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace MVCRouting.Transformers
+{
+    public class SlugifyParameterTransformer : IOutboundParameterTransformer
+    {
+        public string TransformOutbound(object value)
+        {
+            if (value == null) { return null; }
+
+            return Regex.Replace(value.ToString(),
+                                 @"\s",
+                                 "_",
+                                 RegexOptions.CultureInvariant,
+                                 TimeSpan.FromMilliseconds(100)).ToLowerInvariant();
+        }
+    }
+}
